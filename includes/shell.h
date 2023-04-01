54c3e558898f257867652ea275b39c73e87a291b
Arsen <arsen.badalyan441578@gmail.com>
2023-04-01 18:53:41 +0400

    feat: init functions added

--- includes/shell.h
+++ includes/shell.h
@@ -1,27 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shell.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arsbadal <arsbadal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/01 12:25:46 by arsbadal          #+#    #+#             */
+/*   Updated: 2023/04/01 12:35:04 by arsbadal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef SHELL_H
 #define SHELL_H
 
 typedef struct s_minishell {
-    struct s_exc_line *execute;
+	struct s_exc_line *execute;
+	struct s_status *status;
+	char **envp;
 } t_minishell;
 
 typedef struct s_exc_line {
-    struct s_cmd *cmd_list;
-    char *cmd_line;
-    char **cmd_parts;
+	struct s_cmd *cmd_list;
+	char *cmd_line;
+	char **cmd_parts;
 } t_exc_line;
 
-typedef struct s_cmd {
-    char *cmd_line;
-    char **cmd;
-} t_cmd;
+typedef struct s_cmds {
+	char *cmd_line;
+	char **cmd;
+} t_cmds;
 
-typedef struct s_error {
-    int line_status;
-    int part_status;
-} t_error;
+typedef struct s_status {
+	int line;
+	int part;
+	int error;
+} t_status;
 
 // Initialize
 t_minishell *init_minishell();
+t_exc_line *init_exc_line();
+t_cmds **init_cmds(size_t size);
+t_status *init_status();
 
 #endif
--- src/main/mishell.c
+++ src/main/mishell.c
@@ -6,7 +6,7 @@
 /*   By: arsbadal <arsbadal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 17:42:02 by arsbadal          #+#    #+#             */
-/*   Updated: 2023/03/26 17:02:32 by arsbadal         ###   ########.fr       */
+/*   Updated: 2023/04/01 12:36:04 by arsbadal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int main(int argc, char **argv, char **envp)
 	if(argc > 1)
 		exit(1);
 	shell = init_minishell();
-	read_shell(envp, shell);
+	shell->envp = envp;
+	read_shell(shell);
 	return (0);
 }
